d42f55fad1e209fa649417bbeb6e22b7febeb927
AS23Project <a.sigit2303@gmail.com>
2023-06-04 20:38:47 +0700

    Update cat_wordpress.sh

--- storage/app/cipi/cat_wordpress.sh
+++ storage/app/cipi/cat_wordpress.sh
@@ -32,15 +32,15 @@ else
     FULL_PATH=/home/$USER_NAME/web
 fi
 
-wget -P /home/$USER_NAME https://wordpress.org/latest.zip
-unzip /home/$USER_NAME/latest.zip -d $FULL_PATH;
-mv $FULL_PATH/wordpress/* $FULL_PATH;
-cp $FULL_PATH/wp-config-sample.php $FULL_PATH/wp-config.php
+sudo wget -P /home/$USER_NAME https://wordpress.org/latest.zip
+sudo unzip /home/$USER_NAME/latest.zip -d $FULL_PATH;
+sudo mv $FULL_PATH/wordpress/* $FULL_PATH;
+sudo cp $FULL_PATH/wp-config-sample.php $FULL_PATH/wp-config.php
 
-sed -i 's/database_name_here/'$USER_NAME'/g' $FULL_PATH/wp-config.php
-sed -i 's/username_here/'$USER_NAME'/g' $FULL_PATH/wp-config.php
-sed -i 's/password_here/'$DBPASS'/g' $FULL_PATH/wp-config.php
-curl -k https://api.wordpress.org/secret-key/1.1/salt/ >> $FULL_PATH/wp-config.php
+sudo sed -i 's/database_name_here/'$USER_NAME'/g' $FULL_PATH/wp-config.php
+sudo sed -i 's/username_here/'$USER_NAME'/g' $FULL_PATH/wp-config.php
+sudo sed -i 's/password_here/'$DBPASS'/g' $FULL_PATH/wp-config.php
+sudo curl -k https://api.wordpress.org/secret-key/1.1/salt/ >> $FULL_PATH/wp-config.php
 
 sudo chown -R www-data: /home/$USER_NAME/web
 sudo chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/web
